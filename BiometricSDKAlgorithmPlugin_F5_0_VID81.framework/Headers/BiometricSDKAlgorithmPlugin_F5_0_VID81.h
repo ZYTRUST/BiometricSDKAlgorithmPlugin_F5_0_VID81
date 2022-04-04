43c385665671720272b99fce6e820319b29e56c4
ZYTRUST SA <ztmobile@zytrust.com>
2022-04-04 09:44:24 -0500

    version 4.23.0
    version 4.23.0

---
+++ BiometricSDKAlgorithmPlugin_F5_0_VID81.framework/Headers/BiometricSDKAlgorithmPlugin_F5_0_VID81.h
@@ -0,0 +1,15 @@
+//
+//  BiometricSDKAlgorithmPlugin_F5_0_VID81.h
+//  BiometricSDKAlgorithmPlugin_F5_0_VID81
+//
+//  Created by Konrad Szewczyk on 25/02/2020.
+//  Copyright © 2020 IDEMIA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BiometricSDKAlgorithmPlugin_F5_0_VID81.
+FOUNDATION_EXPORT double BiometricSDKAlgorithmPlugin_F5_0_VID81VersionNumber;
+
+//! Project version string for BiometricSDKAlgorithmPlugin_F5_0_VID81.
+FOUNDATION_EXPORT const unsigned char BiometricSDKAlgorithmPlugin_F5_0_VID81VersionString[];
